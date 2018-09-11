daa4527ea246b2ce7804aa7387fec671c997c319
zhongh08 <zhongh08@163.com>
2018-09-11 10:53:27 +0800

    redis cluster

--- src/main/java/com/dachen/springweb/redis/RedisTest.java
+++ src/main/java/com/dachen/springweb/redis/RedisTest.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class TestRedis {
+public class RedisTest {
 
     private static Jedis jedis = RedisUtil.getJedis();
 
