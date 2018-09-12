f62762815cb3a774fa0e7cf541820de72d19bf94
zhongh08 <zhongh08@163.com>
2018-09-12 14:11:36 +0800

    kafka java api

---
+++ src/main/java/com/dachen/springweb/kafka/ProducerDemo.java
@@ -0,0 +1,38 @@
+package com.dachen.springweb.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args){
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "192.168.1.236:9092");
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = null;
+        try {
+            producer = new KafkaProducer<String, String>(properties);
+            for (int i = 0; i < 10; i++) {
+                String msg = "Message " + i;
+                producer.send(new ProducerRecord<String, String>("HelloWorld", msg));
+                System.out.println("Sent:" + msg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            producer.close();
+        }
+
+    }
+
+}
